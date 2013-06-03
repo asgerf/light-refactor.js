5c88a9f8295c91bfe2546195dc12b6c60c000917
Asger Feldthaus <asf@cs.au.dk>
2013-06-03 20:40:28 +0200

    more html scanner

--- src/htmljs.js
+++ src/htmljs.js
@@ -1,20 +1,21 @@
 // htmljs.js: Locates JavaScript in HTML
 // =====================================
 
-module.exports = function(code) {
+var extract = module.exports = function(code) {
 	var state = 0;
 
 	// Lexer states				// examples:
 	var Init = 0;				// ""
 	var OpenTag = 1; 			// "<"
-	var InsideTagName = 1;		// "<foo"
-	var BetweenAttributes = 2;	// "<foo "  "<foo bar=5 "
-	var InsideAttrName = 3;
-	var BeforeAttrValue = 4;
-	var InsideAttrValue = 5;
-	var InsideQuotedAttrValue = 6;
-	var InsideScriptTag = 7;
-	var InsideComment = 8;
+	var InsideTagName = 2;		// "<foo"
+	var BetweenAttributes = 3;	// "<foo "  "<foo bar=5 "
+	var InsideAttrName = 4;
+	var BeforeAttrValue = 5;
+	var InsideAttrValue = 6;
+	var InsideQuotedAttrValue = 7;
+	var InsideScriptTag = 8;
+	var InsideComment = 9;
+	var InsideCData = 10;
 
 	var isCloseTag = false;
 	var isSelfClosingTag = false;
@@ -25,10 +26,15 @@ module.exports = function(code) {
 	var endOfAttrName = -1;
 	var startOfAttrValue = -1;
 	var endOfAttrValue = -1;
+	var startOfScriptBody = -1;
+	var endOfScriptBody = -1;
 
 	// TODO: handle style tags??
 
 	var quote = '\0';
+
+	var isScriptTag = false;
+	var result = [];
 
 	function match(text, start, end) {
 		// TODO: experiment with faster implementations
@@ -68,12 +74,11 @@ module.exports = function(code) {
 				case '\v':
 					break; // stay in OpenTag
 				case '>':
-					state = Init; // error tolerance
+					state = Init; // error tolerance: <>, </>, etc
 					break;
 				default:
 					startOfTagName = i;
 					state = InsideTagName;
-					onEnterTagName();
 					break;
 			}
 			break;
@@ -86,19 +91,28 @@ module.exports = function(code) {
 				case '\t':
 				case '\v':
 					endOfTagName = i;
+					onFinishTagName();
 					state = BetweenAttributes;
 					break;
 				case '>':
 					endOfTagName = i; // TODO: check tag
-					state = Init;
+					onFinishTagName();
+					onFinishTag();
+					state = isScriptTag ? InsideScriptTag : Init;
 					break;
 				case '/':
+					onFinishTagName();
 					isSelfClosingTag = true;
 					state = BetweenAttributes;
 					break;
 				case '-': // check for html comment: <!-- 
-					if (2 == i - startOfTagName && code[startOfTagName] === '!' && code[startOfTagName+1] === '-') {
+					if (match('!-',startOfTagName,i)) {
 						state = InsideComment;
+					}
+					break;
+				case '[': // check for CDATA section: <![CDATA[
+					if (match('![CDATA',startOfTagName,i)) {
+						state = InsideCData;
 					}
 					break;
 				default:
@@ -115,7 +129,8 @@ module.exports = function(code) {
 				case '\v':
 					break;
 				case '>':
-					state = Init; // TODO: check tag
+					onFinishTag();
+					state = isScriptTag ? InsideScriptTag : Init;
 					break;
 				case '/':
 					isSelfClosingTag = true;
@@ -142,6 +157,7 @@ module.exports = function(code) {
 					state = BetweenAttributes;
 					break;
 				case '>':
+					onFinishTag(); // note: we discard this attribute, because we don't care about no-value attributes
 					state = Init; // TODO: check tag
 					break;
 				case '/':
@@ -165,6 +181,7 @@ module.exports = function(code) {
 				case '\v':
 					break;
 				case '>':
+					onFinishTag(); // note: we discard this attribute, because we don't care about no-value attributes
 					state = Init; // TODO: check tag
 					break;
 				case '/':
@@ -194,7 +211,8 @@ module.exports = function(code) {
 				case '\t':
 				case '\v':
 					endOfAttrValue = i;
-					state = BetweenAttributes; // TODO: check attribute
+					onFinishAttr();
+					state = BetweenAttributes;
 					break;
 				case '>':
 					if (code[i-1] === '/') {
@@ -203,8 +221,9 @@ module.exports = function(code) {
 					} else {
 						endOfAttrValue = i;
 					}
-					// TODO: check attribute
-					state = Init; // TODO: check tag
+					onFinishAttr();
+					onFinishTag();
+					state = isScriptTag ? InsideScriptTag : Init;
 					break;
 				case '/': // handle '/' as regular attribute value
 				default:
@@ -215,7 +234,8 @@ module.exports = function(code) {
 
 			case InsideQuotedAttrValue:
 			if (c === quote) {
-				endOfAttrValue = i; // TODO: check attribute
+				endOfAttrValue = i;
+				onFinishAttr();
 				state = BetweenAttributes;
 			} else if (c === '\r' || c === '\n') {
 				state = BetweenAttributes; // bail out if line-break inside attribute value
@@ -237,7 +257,9 @@ module.exports = function(code) {
 							j--;
 							continue loop;
 						case 't':
-							if (match('</scrip', j-7)) {
+							if (match('</scrip', j-7)) { // TODO: what about "< / script" ?
+								endOfScriptBody = j-7;
+								onFinishScript();
 								state = Init;
 							}
 							break loop;
@@ -250,25 +272,33 @@ module.exports = function(code) {
 
 			case InsideComment:
 			if (c === '>') { // end of comment: -->
-				// take care not to match <!--> and <!---> (but <!----> is ok)
+				// take care not to match <!--> and <!---> (but <!----> is ok) (TODO check with browser)
 				if (i - startOfTagName > 3 && code[i-1] === '-' && code[i-2] === '-') {
+					state = Init;
+				}
+			}
+			break;
+
+			case InsideCData:
+			if (c === '>') {
+				if (match(']]',i-2)) {
 					state = Init;
 				}
 			}
 			break;
 		}
 	}
+
 	// TODO: Try ways to avoid closure vars, and compare performance
-
-	var isScriptTag;
 	var scriptTagIsJavaScript;
-	var scriptTagSrc;
+	var startOfScriptSrc;
+	var endOfScriptSrc
 
-	function onEnterTagName() {
+	function onFinishTagName() {
 		if (isCloseTag)
 			return;
 		isScriptTag = match('script', startOfTagName, endOfTagName);
-		scriptTagSrc = null;
+		startOfScriptSrc = endOfScriptSrc = -1;
 		scriptTagIsJavaScript = true;
 	}
 
@@ -277,36 +307,53 @@ module.exports = function(code) {
 			if (match('type', startOfAttrName, endOfAttrName)) {
 				var type = code.substring(startOfAttrValue, endOfAttrValue);
 				if (!/javascript/i.test(type) && !/ecmascript/i.test(type)) {
-					scriptTagIsJavaScript = false; // it's another type of script
+					scriptTagIsJavaScript = false;
 				}
 			}
 			else if (match('src', startOfAttrName, endOfAttrName)) {
-				scriptTagSrc = code.substring(startOfAttrName, endOfAttrName);
+				startOfScriptSrc = startOfAttrValue;
+				endOfScriptSrc = endOfAttrValue;
 			}
 		}
 		tryEventHandler();
 		tryHrefJavaScript();
 	}
+
 	function onFinishTag() {
 		if (isScriptTag) {
-
+			startOfScriptBody = i+1; // TODO: nicer way to read offset?
 		}
+	} // TODO: if we really don't need this, then remove it
+
+	function onFinishScript() {
 		tryScript();
+		isScriptTag = false;
 	}
 
 	function tryScript() {
-		if (!isScriptTag)
+		if (!scriptTagIsJavaScript)
 			return;
-		if (scriptTagSrc !== null) {
-			insideScriptTag = true;
+		if (startOfScriptSrc !== -1) {
+			outputJavaScript({
+				type: "extern",
+				href: {
+					start: startOfScriptSrc,
+					end: endOfScriptSrc
+				}
+			})
 		} else {
-
+			outputJavaScript({
+				type: "script",
+				code: {
+					start: startOfScriptBody,
+					end: endOfScriptBody
+				}
+			})
 		}
 	}
 	
 	function tryEventHandler() {
 		// TODO: handle namespace prefixes?
-		// check that the atribute name starts with "on" (as in, "onload")
 		if (!match('on', startOfAttrName))
 			return;
 		outputJavaScript({
@@ -343,7 +390,21 @@ module.exports = function(code) {
 	}
 
 	function outputJavaScript(obj) {
-		
+		result.push(obj);
+		for (k in obj) { // FIXME: debugging hack to test offsets
+			if (obj[k].start) {
+				obj[k].txt = code.substring(obj[k].start, obj[k].end);
+			}
+		}
 	}
 
+	return result;
 };
+
+// Testing entry point
+if (require.main === module) {
+	var fs = require('fs')
+	var code = fs.readFileSync(process.argv[2], 'utf8');
+	var chunks = extract(code);
+	console.dir(chunks);
+}
